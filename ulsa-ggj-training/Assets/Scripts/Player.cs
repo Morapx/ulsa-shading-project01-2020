d6a05f18c193d77a59540fada73baab96efe0c53
irb <jose_fernando159@hotmail.com>
2020-02-06 11:25:09 -0700

    New

---
+++ ulsa-ggj-training/Assets/Scripts/Player.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    void Update()
+    {
+        Vector2 axis = new Vector2(input.GetAxis("Horizontal"), input.GetAxis("Vertical"));
+
+        transform.Translate(axis * 1f);
+    }
+}
